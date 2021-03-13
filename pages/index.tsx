f63cb5cd644d6113c960830a87eb4e205d8cab50
Ralph Cacho <cacho.rc@gmail.com>
2021-03-13 12:32:02 -0800

    add Loader and Navbar components

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,12 +5,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Link prefetch={false} href={{
-        pathname: '/[username]',
-        query: { username: 'ralphiz' }
-      }}>
-        <a>Ralph's profile</a>
-      </Link>
+
     </div>
   )
 }
