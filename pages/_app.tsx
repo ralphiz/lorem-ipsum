3440704dcf5825727c176491d749dc028bc65ed7
Ralph Cacho <cacho.rc@gmail.com>
2021-03-13 17:29:42 -0800

    add react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 
 import Navbar from "../components/Navbar";
+import { Toaster } from "react-hot-toast";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
+      <Toaster />
     </>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Link from "next/link";
 
 export default function Home() {
   return <div className={styles.container}></div>;
