84b883e3e3204dd6ecaff51a8e56fd7d9914813c
Ralph Cacho <cacho.rc@gmail.com>
2021-03-31 20:43:58 -0700

    feat: start on profile page

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -23,3 +23,30 @@ export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
 export const firestore = firebase.firestore();
 export const storage = firebase.storage();
+
+// Helper functions
+
+/**
+ * Gets a users/{uid} document with username
+ * @param {string} username
+ */
+export async function getUserWithUsername(username) {
+  const usersRef = firestore.collection("users");
+  const query = usersRef.where("username", "==", username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+  return userDoc;
+}
+
+/**
+ * Converts a firestore document to JSON
+ * @param {DocumentSnapshot} doc
+ */
+export function postToJSON(doc: firebase.firestore.DocumentSnapshot) {
+  const data = doc.data();
+  return {
+    ...data,
+    // firestore timestamp NOT serializable to JSON
+    createdAt: data.createdAt.toMillis(),
+    updatedAt: data.updatedAt.toMillis(),
+  };
+}
