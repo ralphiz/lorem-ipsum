8726e90499f2aa33cac398203bc7f230b5dde601
Ralph Cacho <cacho.rc@gmail.com>
2021-07-24 15:33:44 -0700

    Create Metatags component

---
+++ components/Metatags.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function MetaTags({ title, description, image }) {
+  return (
+    <Head>
+      <title>{title}</title>
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:site" content="@ralphiz" />
+      <meta name="twitter:title" content="title" />
+      <meta name="twitter:description" content={description} />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={image} />
+    </Head>
+  );
+}
